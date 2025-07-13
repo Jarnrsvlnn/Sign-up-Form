11fd4e2eb3b7c91e045a491d17b420addad6cae1
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2025-07-13 20:49:41 +0800

    Added some permanent changes such as the positioning of each inputs and proper alignment the background container

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const submitBtn = document.querySelector(".create-btn");
+const dataContent = document.querySelector(".data-container")
+const firstName = document.querySelector("input#first-name")
 
-submitBtn.addEventListener("click", () => alert("INFO SUBMITTED"));
+function handleClickEvent () {
+    const data = document.createElement("h1");
+    data.textContent = `Hello, ${firstName.value}`;
+    dataContent.append(data);
+
+    window.open('landingpage.html', '_blank');
+}
+
+submitBtn.addEventListener("click", handleClickEvent);
